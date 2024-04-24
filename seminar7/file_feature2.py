855acddf171c178fb029ae1fb9241f6868b1b4dc
annakopeikina <anna.v.kopeikina@gmail.com>
2024-04-25 02:21:03 +0400

    commit

--- seminar7/file_feature2.py
+++ seminar7/file_feature2.py
@@ -1,5 +1,4 @@
 
-
-with open('pseudonymous.txt', 'r+', encoding='utf-8') as f:
+with open('mat.txt', 'r+', encoding='utf-8') as f:
     print(list(f))
     #  Менеджер контекста with Open # with -  Гарантирует закрытие файла сохранения информации
